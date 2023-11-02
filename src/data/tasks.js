590f1aef5b33a9f7d1416150758d576829c7a047
Felipe <felitooliva2@gmail.com>
2023-11-02 01:37:06 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primera chamba",
+    description: "mi primera chamba",
+  },
+  {
+    id: 1,
+    title: "mi segunda chamba",
+    description: "mi segunda chamba",
+  },
+  {
+    id: 2,
+    title: "mi tercera chamba",
+    description: "mi tercera chamba",
+  },
+];
